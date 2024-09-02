3374a81abafd6e7331b3cd6a74a6897baea0136c
Hawa2109 <hawakone831@gmail.com>
2024-09-02 13:09:16 +0200

    mo

---
+++ JEUX/script.js
@@ -0,0 +1,4 @@
+document.getElementById("startButton").addEventListener("click", function () {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("challengeList").style.display = "block";
+});
